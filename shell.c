c56c6313348d4eb5a8132d492dd102db4852411b
Batumt <batumyusuf09@gmail.com>
2025-12-01 00:14:59 +0300

    Initial commit: basic shell implementation

---
+++ shell.c
@@ -0,0 +1,125 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include "./lib/linenoise.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+#define PROMPT "$ "
+#define HISTORY_LENGTH 1024
+#define MAX_ARGS 1024
+#define TOKEN_SEP " \t"
+#define PATH_MAX 4096
+
+char CWD[PATH_MAX];
+
+int s_read(char *input, char **args) {
+    int i = 0;
+    char *token = strtok(input, TOKEN_SEP);
+    while (token != NULL && i < (MAX_ARGS - 1)) {
+        args[i++] = token;
+        token = strtok(NULL, " \t");
+    }
+    args[i] = NULL;
+    return i;
+}
+
+
+int s_execute(char *cmd, char **cmd_args) {
+  fprintf(stdout, "Executing `%s`!\n", cmd);
+
+  int status;
+  pid_t pid;
+
+  pid = fork();
+  if (pid < 0) {
+    fprintf(stderr, "Could not execute!\n");
+    return -1;
+  }
+
+  if (pid == 0) {
+    execvp(cmd, cmd_args);
+  } else {
+    //Wait
+    if (waitpid (pid, &status, 0) != pid) {
+      fprintf(stderr, "Could not wait!\n");
+      return -1;
+    }
+  }
+
+  return status;  
+}
+
+typedef enum Builtin {
+  CD,
+  PWD,
+  INVALID
+} Builtin;
+
+void builtin_impl_cd(char **args, size_t n_args);
+void builtin_impl_pwd(char **args, size_t n_args);
+
+void (*BUILTIN_TABLE[]) (char **args, size_t n_args) = {
+  [CD] = builtin_impl_cd,
+  [PWD] = builtin_impl_pwd,
+};
+
+Builtin builtin_code(char *cmd) {
+    if (!strncmp(cmd, "cd", 2)) {
+        return CD;
+    } else if (!strncmp(cmd, "pwd", 3)) {
+        return PWD;
+    } else {
+        return INVALID;
+    }
+}
+
+
+
+
+int is_builtin(char *cmd) {
+    return builtin_code(cmd) != INVALID;
+}
+
+
+
+void s_execute_builtin(char *cmd, char **args, size_t n_args) {
+    BUILTIN_TABLE[builtin_code(cmd)](args, n_args);
+}
+
+
+
+int main(void) {
+    if (! linenoiseHistorySetMaxLen(HISTORY_LENGTH)) {
+        fprintf(stderr, "Could not set linenoise history!");
+        exit(1);
+    }
+
+    char *line;
+    char *args[MAX_ARGS];
+    while((line = linenoise(PROMPT)) != NULL) {
+    
+    //Read step        
+
+    int args_read = s_read(line, args);
+    fprintf(stdout, "Read %d args\n", args_read);    
+    for (int i = 0; i < args_read; i++) {
+        fprintf(stdout, "arg[%d] = %s\n", i, args[i]);
+    }
+
+// Skip empty lines
+    if (args_read == 0) {
+        linenoiseFree(line);
+        continue;
+    }
+
+    char *cmd = args[0];
+    char **cmd_args = args;
+    s_execute(cmd, cmd_args);
+    linenoiseHistoryAdd(line);
+    linenoiseFree(line);
+    }
+    
+    return 0;
+} 
